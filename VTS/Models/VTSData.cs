684fc227fc11cdca49b720d4db5d0a1d54e540b1
Millyarde <admin@millyarde.com>
2023-01-22 07:53:42 -0600

    Fix Typos

--- VTS/Models/VTSData.cs
+++ VTS/Models/VTSData.cs
@@ -97,7 +97,7 @@ namespace VTS.Models {
     [System.Serializable]
     public class VTSFolderInfoData : VTSMessageData {
          public VTSFolderInfoData(){
-            this.messageType = "VTSFolderInfoRequestuest";
+            this.messageType = "VTSFolderInfoRequest";
             this.data = new Data();
         }
         public Data data;
@@ -554,7 +554,7 @@ namespace VTS.Models {
 		    public int baseStrength;
 		    public int baseWind;
 		    public bool apiPhysicsOverrideActive;
-		    public string apiPhysicsOverridePluginNam;
+		    public string apiPhysicsOverridePluginName;
 		    public VTSPhysicsGroup[] physicsGroups;
         }
     }
