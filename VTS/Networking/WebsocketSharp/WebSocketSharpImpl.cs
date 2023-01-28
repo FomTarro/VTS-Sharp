316e8f2e197be041d32ef45b63dfb8720db57d1f
Tom Farro <twfarro@wpi.edu>
2023-01-28 16:28:44 -0500

    Logging now logs socket host instead of full socket URL

--- VTS/Networking/WebsocketSharp/WebSocketSharpImpl.cs
+++ VTS/Networking/WebsocketSharp/WebSocketSharpImpl.cs
@@ -48,20 +48,20 @@ namespace VTS.Networking.Impl {
             //     // this._socket.Close();
             // }
             this._socket = new WebSocket(this._url);
-            Debug.Log(string.Format("Attempting to connect to {0}", this._socket.Url));
+            Debug.Log(string.Format("Attempting to connect to {0}", this._socket.Url.Host));
             this._socket.WaitTime = TimeSpan.FromSeconds(10);
             this._socket.Log.Output = (l, m) => {
                 switch(l.Level){
                     case LogLevel.Fatal:
                     case LogLevel.Trace:
                     case LogLevel.Error:
-                        Debug.LogError(string.Format("[{0}] - Socket error: {1}", this._socket.Url, l.Message));
+                        Debug.LogError(string.Format("[{0}] - Socket error: {1}", this._socket.Url.Host, l.Message));
                         break;
                     case LogLevel.Warn:
-                        Debug.LogError(string.Format("[{0}] - Socket warning: {1}", this._socket.Url, l.Message));
+                        Debug.LogError(string.Format("[{0}] - Socket warning: {1}", this._socket.Url.Host, l.Message));
                         break;
                     default:
-                        Debug.LogError(string.Format("[{0}] - Socket info: {1}", this._socket.Url, l.Message));
+                        Debug.LogError(string.Format("[{0}] - Socket info: {1}", this._socket.Url.Host, l.Message));
                         break;
                 }
             };
@@ -78,13 +78,13 @@ namespace VTS.Networking.Impl {
             this._socket.OnOpen += (sender, e) => { 
                 MainThreadUtil.Run(() => {
                     this._onConnect();
-                    Debug.Log(string.Format("[{0}] - Socket open!", this._socket.Url));
+                    Debug.Log(string.Format("[{0}] - Socket open!", this._socket.Url.Host));
                     this._attemptReconnect = true;
                 });
             };
             this._socket.OnError += (sender, e) => { 
                 MainThreadUtil.Run(() => {
-                    Debug.LogError(string.Format("[{0}] - Socket error...", this._socket.Url));
+                    Debug.LogError(string.Format("[{0}] - Socket error...", this._socket.Url.Host));
                     if(e != null){
                         Debug.LogError(string.Format("'{0}', {1}", e.Message, e.Exception));
                     }
@@ -93,7 +93,7 @@ namespace VTS.Networking.Impl {
             };
             this._socket.OnClose += (sender, e) => { 
                 MainThreadUtil.Run(() => {
-                    string msg = string.Format("[{0}] - Socket closing: {1}, '{2}', {3}", this._socket.Url, e.Code, e.Reason, e.WasClean);
+                    string msg = string.Format("[{0}] - Socket closing: {1}, '{2}', {3}", this._socket.Url.Host, e.Code, e.Reason, e.WasClean);
                     if(e.WasClean){
                         Debug.Log(msg);
                         this._onDisconnect();
