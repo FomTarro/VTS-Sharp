672e772167d6c500ae0db116bb3fabb0b549a3f9
Hogo <64896329+HugoVG@users.noreply.github.com>
2023-11-26 16:47:24 +0100

    Expanded Core examples
    - Simple Example to show how to get everything up and going
    - Advanced Example to show to make a more robust plugin

---
+++ Examples/Core/Advanced/AdvancedPlugin/AdvancedPlugin/Services/VTSLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using VTS.Core;
+
+namespace AdvancedPlugin.Services;
+
+//Singleton
+
+public class VTSLogger : IVTSLogger
+{
+    private readonly ILogger<VTSLogger> _logger;
+
+    public VTSLogger(ILogger<VTSLogger> logger)
+    {
+        _logger = logger;
+    }
+
+    public void Log(string message) => _logger.LogInformation(message);
+
+    public void LogError(string error) => _logger.LogError(error);
+
+    public void LogError(Exception error) => _logger.LogError(error, error.Message);
+
+    public void LogWarning(string warning) => _logger.LogWarning(warning);
+}
---
+++ Examples/Core/Simple/SimplePlugin/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Hosting;
+using VTS.Core;
+
+// This is a simple example of how to use the VTS plugin in C#.
+// You can use this as a starting point for your own plugin implementation
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+ConsoleVTSLoggerImpl logger = new();
+WebSocketImpl websocket = new(logger);
+NewtonsoftJsonUtilityImpl jsonUtility = new();
+TokenStorageImpl tokenStorage = new("");
+
+CoreVTSPlugin plugin = new(logger, 100, "My simple plugin", "Perfect Programmer", "");
+
+try {
+    await plugin.InitializeAsync(websocket, jsonUtility, tokenStorage, () => logger.LogWarning("Disconnected!"));
+    logger.Log("Connected!");
+
+    var apiState = await plugin.GetAPIState();
+    logger.Log("Using VTubeStudio " + apiState.data.vTubeStudioVersion);
+
+    var currentModel = await plugin.GetCurrentModel();
+    logger.Log("The current model is: " + currentModel.data.modelName);
+    
+    // Subscribe to your events here using the plugin.SubscribeTo* methods
+    
+    await plugin.SubscribeToBackgroundChangedEvent((backgroundInfo) => {
+        logger.Log("The background was changed to: " + backgroundInfo.data.backgroundName);
+    });
+    
+    // To unsubscribe, use the plugin.UnsubscribeFrom* methods
+    
+} catch (VTSException e) {
+    logger.LogError(e);
+}
+
+var host = builder.Build(); // Build the host
+
+await host.RunAsync(); // This will keep the program running until the user presses Ctrl+C, or kills the process
+
+
+
